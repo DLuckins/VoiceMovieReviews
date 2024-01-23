6cb9209809642295fae3264b1ee6ef6c59723f72
DLuckins <99718812+DLuckins@users.noreply.github.com>
2024-01-23 16:51:32 +0200

    Initialized the project

---
+++ VoiceReview/src/main/java/com/example/VoiceReview/VoiceReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.VoiceReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceReviewApplication.class, args);
+	}
+
+}
